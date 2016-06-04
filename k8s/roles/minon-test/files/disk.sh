4332850f3cb770d8c74741d54b4f3bc0f4d6654e
chloroplast <41893204@qq.com>
2016-06-04 11:37:34 +0800

    更新 k8s-minon 剧本

---
+++ k8s/roles/minon-test/files/disk.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISK=$1
+
+CHECK_EXIST=`fdisk -l 2> /dev/null | grep -o "$DISK"`
+[ ! "$CHECK_EXIST" ] && { echo "Error: Disk is not found !"; exit 1;}
+
+CHECK_DISK_EXIST=`fdisk -l 2> /dev/null | grep -o "$DISK[1-9]"`
+[ ! "$CHECK_DISK_EXIST" ] || { echo "WARNING: ${CHECK_DISK_EXIST} is Partition already !"; exit 0;}
+
+fdisk -S 56 $DISK<<EOF  
+n
+p
+1
+
+
+wq
+EOF
