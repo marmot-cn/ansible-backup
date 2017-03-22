0af1920aaba1fe31e3687e6b3340b80711eb6f48
chloroplast <41893204@qq.com>
2017-03-22 10:24:16 +0800

    更新security脚本

--- security/security.sh
+++ security/security.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-ssh root@$1 "useradd ansible;passwd ansible;usermod -aG wheel  ansible;chmod 755 /etc/sudoers;sed -i '105 s/^/# /g' /etc/sudoers;sed -i '108 s/# //g' /etc/sudoers;chmod 440 /etc/sudoers"
+ssh root@$1 "useradd ansible;passwd ansible;usermod -aG wheel  ansible;chmod 755 /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tALL/s/^/#/1' /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tNOPASSWD/s/# //1' /etc/sudoers;chmod 440 /etc/sudoers"
 
 cat ~/.ssh/id_rsa.pub | ssh ansible@$1 "umask 077; mkdir -p .ssh ; cat >> .ssh/authorized_keys"
