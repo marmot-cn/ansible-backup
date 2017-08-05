e5bcf7782239af315db2674b46ce69b29c00032f
chloroplast <chloroplast@chloroplastdeMacBook-Pro.local>
2017-08-05 12:50:53 +0800

    不明文输入密码

--- security/security.sh
+++ security/security.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-ssh root@$1 "useradd ansible;passwd ansible;usermod -aG wheel  ansible;chmod 755 /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tALL/s/^/#/1' /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tNOPASSWD/s/# //1' /etc/sudoers;chmod 440 /etc/sudoers"
+read -p "Enther Server ip:" ip
+read -s -p "Enther ansible password:" password
 
-cat ~/.ssh/id_rsa.pub | ssh ansible@$1 "umask 077; mkdir -p .ssh ; cat >> .ssh/authorized_keys"
+echo ""
+
+ssh root@$ip "useradd ansible;echo $password | passwd --stdin ansible;usermod -aG wheel  ansible;chmod 755 /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tALL/s/^/#/1' /etc/sudoers;sed -i '/\%wheel\tALL=(ALL)\tNOPASSWD/s/# //1' /etc/sudoers;chmod 440 /etc/sudoers"
+
+cat ~/.ssh/id_rsa.pub | ssh ansible@$ip "umask 077; mkdir -p .ssh ; cat >> .ssh/authorized_keys"
