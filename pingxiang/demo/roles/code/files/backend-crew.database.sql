1302a61778e436488fb2cc4e8a4cf0de14fc169a
chloroplast <chloroplast@chloroplastdeMacBook-Pro.local>
2017-08-15 12:06:12 +0800

    pingxiang demo部署脚本

--- pingxiang/demo/roles/code/files/backend-crew.database.sql
+++ pingxiang/demo/roles/code/files/backend-crew.database.sql
@@ -28,4 +28,11 @@ ALTER TABLE `pcore_user`
 
 ALTER TABLE `pcore_user` ADD `avatar` VARCHAR(255) NULL COMMENT '头像' AFTER `organization_id`;
 
-INSERT INTO pcore_user (user_id,cellphone,user_name,real_name,password,salt,email,avatar,organization_id,create_time,update_time,status,status_time) VALUES ('0','18808060165','administrator','杨勋','6c506c682c84e59aba07eff0c319f82d','YKFI','1361600985@qq.com','','1','1501906120','1501906120','0','1501906120');
+insert into `pcore_user`(`user_id`,`cellphone`,`password`,`salt`,`create_time`,`update_time`,`status`,`status_time`,`real_name`,`user_name`,`email`,`organization_id`,`avatar`) values
+('74','15191887088','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501905251','1501905251','0','1501905251','政府金融','zfjrb','1361500925@qq.com','13',null),
+('73','15191887078','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501905183','1501905183','0','1501905183','人力社保','rlsbj','1361500935@qq.com','2',null),
+('67','15191887068','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501905018','1501905018','0','1501905018','国资委','gzw','1361500945@qq.com','20',null),
+('66','15191887058','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501904908','1501904908','0','1502011532','国土资源','gtzy','1361500955@qq.com','3',null),
+('65','15191887048','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501904834','1502011717','0','1501904834','国税局','gsj','1361500965@qq.com','8',''),
+('64','15191887028','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501904734','1501904734','0','1502011777','发改委','fgw','1361500975@qq.com','1',null),
+('2','18729358585','9acebf99215085a5ea0c2e5e62f12199','HXlq','1501231018','1501558121','0','1501919528','管理员','admin','123456789@qq.com','1',null);
